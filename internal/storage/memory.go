c45a1b4726cc3d9dfb40134eaa4decd729fcd46f
Igor Guryan <sacujo93@yandex.ru>
2026-07-06 13:45:51 +0300

    feat(storage): implement in-memory link storage

--- internal/storage/memory.go
+++ internal/storage/memory.go
@@ -1,6 +1,12 @@
 package storage
 
-import "url-shortener/internal/model"
+import (
+	"errors"
+	"url-shortener/internal/model"
+)
+
+var ErrNotFound = errors.New("link not found")
+var ErrAlreadyExists = errors.New("link already exists")
 
 type MemoryStorage struct {
 	links map[string]model.Link
@@ -12,6 +18,18 @@ func NewMemoryStorage() *MemoryStorage {
 	}
 }
 
-func (s *MemoryStorage) Save(link model.Link) {
+func (s *MemoryStorage) Save(link model.Link) error {
+	if _, exists := s.links[link.ID]; exists {
+		return ErrAlreadyExists
+	}
 	s.links[link.ID] = link
+	return nil
+}
+
+func (s *MemoryStorage) FindByID(id string) (model.Link, error) {
+	link, exists := s.links[id]
+	if !exists {
+		return model.Link{}, ErrNotFound
+	}
+	return link, nil
 }
